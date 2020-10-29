1443c550ef1937e167e2f1350e8f51731ea55466
eran <eran505@gmail.com>
2020-10-29 08:38:57 +0200

    10/29 08:38:57

--- visualization/vizo.py
+++ visualization/vizo.py
@@ -12,36 +12,38 @@ def get_cmap(n, name='hsv'):
     '''Returns a function that maps each index in 0, 1, ..., n-1 to a distinct
     RGB color; the keyword argument name must be a standard mpl colormap name.'''
     return plt.cm.get_cmap(name, n)
-cmap = get_cmap(15)
-l = hlp.load__p("/home/ERANHER/car_model/debug/p.csv")
 
-print(l)
-matrix = []
-for item in l:
-    size = len(item["traj"])
-    path=np.zeros([size,3])
-    for idx,pos in enumerate(item["traj"]):
-        path[idx]=pos[0]
-    matrix.append(path)
+def main_f():
+    cmap = get_cmap(15)
+    l = hlp.load__p("{}/car_model/debug/1000000_p.csv".format(expanduser("~")))
 
-#exit(0)
-fig = plt.figure()
-ax = plt.axes(projection='3d')
-#ax = plt.axes(projection='3d')
+    print(l)
+    matrix = []
+    for item in l:
+        size = len(item["traj"])
+        path = np.zeros([size, 3])
+        for idx, pos in enumerate(item["traj"]):
+            path[idx] = pos[0]
+        matrix.append(path)
 
-# Data for a three-dimensional line
-color = ['red', 'green', 'gold', 'black', 'lime', 'violet', 'plum',
-         'orange','navy','salmon']
-for i,p in enumerate(matrix):
-    zline = np.array(p[:,2])
-    xline = np.array(p[:,1])
-    yline = np.array(p[:,0])
-    ax.plot3D(xline, yline, zline,color[i%len(color)])
+    # exit(0)
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+    # ax = plt.axes(projection='3d')
 
-# Data for three-dimensional scattered points
-# zdata = 15 * np.random.random(100)
-# xdata = np.sin(zdata) + 0.1 * np.random.randn(100)
-# ydata = np.cos(zdata) + 0.1 * np.random.randn(100)
-# ax.scatter3D(xdata, ydata, zdata, c=zdata, cmap='Greens')
-plt.savefig("{}/car_model/tmp.png".format(expanduser("~")))
-plt.show()
+    # Data for a three-dimensional line
+    color = ['red', 'green', 'gold', 'black', 'lime', 'violet', 'plum',
+             'orange', 'navy', 'salmon']
+    for i, p in enumerate(matrix):
+        zline = np.array(p[:, 2])
+        xline = np.array(p[:, 1])
+        yline = np.array(p[:, 0])
+        ax.plot3D(xline, yline, zline, color[i % len(color)])
+
+    ax.view_init(azim=0, elev=100)
+
+    plt.savefig("{}/car_model/tmp.png".format(expanduser("~")))
+    plt.show()
+
+if __name__ == "__main__":
+   main_f()
